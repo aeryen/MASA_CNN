8dad42bb83b7e01a9f7cd082603acaa24b798e2c
aeryen <aeryen@gmail.com>
2017-08-08 04:56:22 -0500

    logger works in evaler now

--- evaluators/eval_a_origin.py
+++ evaluators/eval_a_origin.py
@@ -19,8 +19,9 @@ class EvaluatorOrigin:
 
     @staticmethod
     def get_exp_logger(exp_dir, checkpoing_file_name):
-        log_path = exp_dir + checkpoing_file_name + "_eval.log"
+        log_path = os.path.join(exp_dir, checkpoing_file_name + "_eval.log")
         # logging facility, log both into file and console
+        logging.getLogger('').handlers = []
         logging.basicConfig(level=logging.DEBUG,
                             format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                             datefmt='%m-%d %H:%M',
@@ -36,7 +37,6 @@ class EvaluatorOrigin:
         else:
             checkpoint_file = tf.train.latest_checkpoint(experiment_dir + "/checkpoints/", latest_filename=None)
         file_name = os.path.basename(checkpoint_file)
-        EvaluatorOrigin.get_exp_logger(exp_dir=experiment_dir, checkpoing_file_name=file_name)
 
         logging.info("Evaluating: " + __file__)
         logging.info("Test for prob: " + self.dater.problem_name)
@@ -81,8 +81,7 @@ class EvaluatorOrigin:
         # all_predictions = np.array(all_predictions)
         # average_accuracy = all_predictions.sum(axis=0) / float(all_predictions.shape[0])
         average_accuracy = correct_predictions / len(all_predictions)
-        logging.info("Total number of test examples: {}".format(len(self.test_data.label_instance)))
-        logging.info("\t" + str(average_accuracy))
+        logging.info("ACC\t" + str(average_accuracy))
 
         mse = mean_squared_error(y_true=index_label, y_pred=all_predictions)
         logging.info(("MSE\t" + str(mse)))
@@ -90,9 +89,15 @@ class EvaluatorOrigin:
 
 
 if __name__ == "__main__":
-    dater = DataHelperHotelOne(embed_dim=300, target_sent_len=1024, target_doc_len=None,
-                               aspect_id=1, doc_as_sent=True)
-    ev = EvaluatorOrigin(dater=dater)
+    experiment_dir = "C:\\Users\\aeryen\\Desktop\\MASA_CNN\\runs\\TripAdvisor_Origin\\170731_1501526605"
+    checkpoint_steps = [5000]
 
-    ev.evaluate(experiment_dir="C:\\Users\\aeryen\\Desktop\\MASA_CNN\\runs\\TripAdvisor_Origin\\170731_1501493057",
-                checkpoint_step=4000)
+    for step in checkpoint_steps:
+        EvaluatorOrigin.get_exp_logger(exp_dir=experiment_dir, checkpoing_file_name=str(step))
+
+        dater = DataHelperHotelOne(embed_dim=300, target_sent_len=1024, target_doc_len=None,
+                                   aspect_id=1, doc_as_sent=True)
+        ev = EvaluatorOrigin(dater=dater)
+
+        ev.evaluate(experiment_dir=experiment_dir,
+                    checkpoint_step=step)
